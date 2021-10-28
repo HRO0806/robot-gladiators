adef22d99e023ebdc95a1d8640ae33168585d7be
HRO0806 <henryolson@icloud.com>
2021-10-28 16:14:45 -0500

    MVP done

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,12 +68,20 @@ var fight = function(enemyName) {
         break;
       } else {
         window.alert(playerName + ' still has ' + playerHealth + ' health left.');
+      
       }
     }
   };
 
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators!  Round " + (i + 1) );
+    
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;   
     fight(pickedEnemyName);
+  } else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
 };
